52793361b7c132ab84f833d67e5ec921055cb850
James Madden <jamesbmadden@gmail.com>
2023-02-07 12:32:15 -0500

    Add Swap Chain Panel to play page to render to

--- PlayPage.xaml.cpp
+++ PlayPage.xaml.cpp
@@ -13,25 +13,16 @@ using namespace Microsoft::UI::Xaml;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace winrt::DirectGB::implementation
-{
-    PlayPage::PlayPage()
-    {
-        InitializeComponent();
-    }
+namespace winrt::DirectGB::implementation {
+  PlayPage::PlayPage() {
+    InitializeComponent();
+  }
 
-    int32_t PlayPage::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
+  int32_t PlayPage::MyProperty() {
+    throw hresult_not_implemented();
+  }
 
-    void PlayPage::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void PlayPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
-    }
+  void PlayPage::MyProperty(int32_t /* value */) {
+    throw hresult_not_implemented();
+  }
 }
